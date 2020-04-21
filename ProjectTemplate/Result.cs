6e5ecf6629e91442dc7e22bdda814e74761f9616
kevin <kshum1@asu.edu>
2020-04-20 22:06:05 -0700

    added more

---
+++ ProjectTemplate/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Result
+    {
+        public string firstName;
+        public string lastName;
+        public int numOfClasses;
+        public int time;
+        public int numOfSubjects;
+        public int numOfAttendees;
+    }
+}
---
+++ ProjectTemplate/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class User
+    {
+        public String First;
+        public String Last;
+        public String Email;
+        public String URL;
+        public String MentorStatus;
+        public String Skills;
+        public String Assistance;
+        public String Bio;
+        public String AccountStatus;
+
+
+    }
+}
